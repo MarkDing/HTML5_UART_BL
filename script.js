f69dce8f57774021bdac597f46941d7d9af18124
Mark <xiaoxid@163.com>
2013-11-20 15:45:29 +0800

    Read file  from file dialog and output to console.

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-var gui = require('nw.gui');
 var fs = require('fs');
-var clipboard = gui.Clipboard.get();
+
+function handleFiles(files) {
+	fs.readFile(files[0].name, 'utf8', function(err,data) {
+		if (err) throw err;
+		console.log(data);
+	});
+}
 
 function openFileOption() {
 	document.getElementById("openFile").click();
 }
 
-// onload = function() {
-// 	openButton = document.getElementById('openHexFile');
-// 	openButton.onClick(openFileOption());
-
-// //	openButton.addEventListener('click', openFileOption);
-// };
-
-//onclick=openFileOption()
