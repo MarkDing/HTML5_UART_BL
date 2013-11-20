0231c00babd045d0732e5a7503cb8d897ba8a1b3
Mark <xiaoxid@163.com>
2013-11-20 18:51:42 +0800

    Add Hex file parse code
    It can parse data correctly from Hex file.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 var fs = require('fs');
+var hexParse = require('./intel-hex');
+const FLASH_SIZE = 32768;
+const FLASH_PAGE_SIZE = 512; // 1024
+const FLASH_PAGE_NUM = (FLASH_SIZE / FLASH_PAGE_SIZE);
 
 function handleFiles(files) {
 	fs.readFile(files[0].name, 'utf8', function(err,data) {
 		if (err) throw err;
-		console.log(data);
+		//console.log(data);
+		var hex = hexParse.parse(data);
+		for (i = 0; i < FLASH_PAGE_NUM; i++) {
+			if (hex.pageInUse[i] == 1) {
+				var addr = i * FLASH_PAGE_SIZE;
+				var tmp = new Buffer(FLASH_PAGE_SIZE);
+				//var tmp = new Array(FLASH_PAGE_SIZE);
+				hex.data.copy(tmp, 0, addr, addr + FLASH_PAGE_SIZE);
+				console.log(tmp);
+			}
+		}
 	});
 }
 
